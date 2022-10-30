0e7b8b28865b73d8231b1057d577a8874cbe884f
Esdras Gomes <esdrasgoomes@gmail.com>
2022-10-29 23:45:19 -0300

    Update script.js

--- quiz/script.js
+++ quiz/script.js
@@ -1,6 +1,6 @@
 // Initial Data
 
-let currentQuestion = 0;
+let currentQuestion = 8;
 
 showQuestion();
 
@@ -9,8 +9,35 @@ function showQuestion() {
     if (questions[currentQuestion]) {
         let q = questions[currentQuestion];
 
-        console.log(q.question);
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+        document.querySelector('.options').innerHTML = '';
+
+        let optionsHtml = '';
+        for (let i in q.options) {
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        });
+
+        // console.log(q.question);
     } else {
         // Acabaram as questões.
+    }
+}
+
+function optionClickEvent(e) {
+    let clickedOption = parseInt(e.target.getAttribute('data-op'));
+
+    // Verificando se a opção clicada é a correta (answer)
+    if (questions[currentQuestion].answer === clickedOption) {
+        console.log("Acertou!");
+    } else {
+        console.log("Errou!");
     }
 }
