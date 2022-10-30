bb53cd5421d30bb7e166aebf5086f4009d530e46
Esdras Gomes <esdrasgoomes@gmail.com>
2022-10-30 12:08:48 -0300

    Updated project

--- quiz/script.js
+++ quiz/script.js
@@ -1,13 +1,22 @@
-// Initial Data
+// Variáveis
 
 let currentQuestion = 0;
+let correctAnswers = 0;
 
 showQuestion();
 
-// Functions
+// Eventos
+// Resetando o quiz ao concluí-lo
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
+
+// Funções
 function showQuestion() {
     if (questions[currentQuestion]) {
         let q = questions[currentQuestion];
+
+        // Calculo referente a barra de porcentagem
+        let porcentagem = Math.floor((currentQuestion / questions.length) * 100);
+        document.querySelector('.progress--bar').style.width = `${porcentagem}%`;
 
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
@@ -24,10 +33,8 @@ function showQuestion() {
         document.querySelectorAll('.options .option').forEach(item => {
             item.addEventListener('click', optionClickEvent);
         });
-
-        // console.log(q.question);
     } else {
-        // Acabaram as questões.
+        finishQuiz();
     }
 }
 
@@ -36,8 +43,42 @@ function optionClickEvent(e) {
 
     // Verificando se a opção clicada é a correta (answer)
     if (questions[currentQuestion].answer === clickedOption) {
-        console.log("Acertou!");
+        correctAnswers++;
     } else {
-        console.log("Errou!");
+
     }
+
+    // Depois que responder a pergunta atual, passar para a próxima pergunta
+    currentQuestion++;
+    showQuestion();
+}
+
+// Função para mostrar as informações ao finalizar o quiz
+function finishQuiz() {
+    let points = Math.floor((correctAnswers / questions.length) * 100);
+
+    if (points <= 30) {
+        document.querySelector('.scoreText1').innerHTML = `Não foi dessa vez!`;
+        document.querySelector('.scorePorcentagem').style.color = '#FF0000';
+    } else if (points > 30 && points < 70) {
+        document.querySelector('.scoreText1').innerHTML = `Estude um pouco mais!`;
+        document.querySelector('.scorePorcentagem').style.color = '#FFFF00';
+    } else if (points >= 70) {
+        document.querySelector('.scoreText1').innerHTML = `Parabéns!`;
+        document.querySelector('.scorePorcentagem').style.color = '#0D630D';
+    }
+
+    document.querySelector('.scorePorcentagem').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}.`;
+
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = '100%';
+}
+
+// Função para resetar o quiz ao concluí-lo
+function resetEvent() {
+    correctAnswers = 0;
+    currentQuestion = 0;
+    showQuestion();
 }
